1a3ae091e3037430b2f36e2e2eca1e77f5aef3c1
Joseph Vidal <jospeh.vidal@epitech.eu>
2018-02-24 23:29:07 +0100

    Adding missing parameters of te clean() function

--- arspoof.py
+++ arspoof.py
@@ -32,6 +32,7 @@ def clean():
     trame = ARP(op = 2, pdst = ip_2, hwdst = get_mac(ip_1))
     print("[\033[32m+\033[00m] Cleaning target's cache")
     send(trame, count = 5)
+    print ("[\033[32m+\033[00m] Quitting")
 
 regex_ip = "([0-9]{1,3}\.){3}[0-9]{1,3}"
 regex_mac = "([0-9a-f]{2}\:){5}[0-9a-f]{2}"
@@ -39,11 +40,11 @@ regex_mac = "([0-9a-f]{2}\:){5}[0-9a-f]{2}"
 if (len(sys.argv) != 3 or sys.argv[1] == "-h"):
     print("USAGE:\n\t./arspoof.py <ip_target> <ip_victim>")
     exit(0)
-if (os.getuid() != 0):
-    os.write(2, "[\033[31m-\033[00m] The script must be run as root.\n")
-    exit(84)
 if (re.match(regex_ip, sys.argv[1]) == None or re.match(regex_ip, sys.argv[2]) == None):
     os.write(2, "[\033[31m-\033[00m] Error: ip adress must match with the regular expression.\n")
+    exit(84)
+if (os.getuid() != 0):
+    os.write(2, "[\033[31m-\033[00m] The script must be run as root.\n")
     exit(84)
 
 ip_1 = sys.argv[1]
@@ -51,4 +52,4 @@ ip_2 = sys.argv[2]
 
 print("[\033[33m!\033[00m] Dont forget to enable forwarding: echo 1 > /proc/sys/net/ipv4/ip_forward")
 arspoof(ip_1, ip_2, regex_mac)
-clean()
+clean(ip_1, ip_2)
